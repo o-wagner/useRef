dd3b12dad2718f81fcd59539d73395e8f3939154
seu_nome_de_usuario <owagner.oliveira@hotmail.com>
2023-05-09 13:06:04 -0300

    useRef example

--- App.js
+++ App.js
@@ -1,11 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useRef, useEffect } from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
+  const [count, setCount] = useState(0);
+  const [number, setNumber] = useState(0);
+
+  const numberRef = useRef(0);
+  const oldNumberRef = useRef();
+  
+ // Não re-renderiza o componente  
+  useEffect(() => {
+    numberRef.current = Math.random();
+    // setNumber((prevNumber) => prevNumber + 10);
+  })
+  
+ // Referência  do valor anterior
+  useEffect(() => {
+    oldNumberRef.current = count;
+
+    // Monitora a alteração do Count V
+  }, [count])
+ 
+ 
+  console.log(numberRef)
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{color: 'white'}}>O number é: {number} </Text>
+      <Text style={{color: 'white'}}>O numero do UseRef é: {numberRef.current} </Text>
+      <Text style={{color: 'white'}}>O contador anterior era: {oldNumberRef.current} </Text>
+     <Text style={{color: 'white'}}>O contador é: {count} </Text>
+      <TouchableOpacity  onPress={() => setCount(count+1)} >
+        <Text style={{ padding: 15, borderRadius:8, marginTop: 10, backgroundColor: '#505050', color: '#00c92b', fontSize: 20}}>Incrementar</Text>
+        </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +42,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000000',
     alignItems: 'center',
     justifyContent: 'center',
+    color: '#555555',
+  
   },
 });
